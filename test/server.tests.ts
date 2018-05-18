67e64294b899e501a125f5702f8fae77d505e975
Justin <greyepoxy2@gmail.com>
2018-05-17 22:15:48 -0700

    test: switch to using ava and make test load module correctly

--- test/server.tests.ts
+++ test/server.tests.ts
@@ -1,19 +1,18 @@
+import { test } from 'ava';
 import { create as createServer } from './fixtures/server/index';
 
 const mockFileName = 'main.ts';
 
-describe('test server', () => {
-  it('should work', () => {
-    const server = createServer();
-    server.openMockFile(mockFileName, 'const q = Array.');
-    server.send({
-      command: 'completions',
-      arguments: { file: mockFileName, offset: 16, line: 1 }
-    });
+test('test server should work', t => {
+  const server = createServer();
+  server.openMockFile(mockFileName, 'const q = Array.');
+  server.send({
+    command: 'completions',
+    arguments: { file: mockFileName, offset: 16, line: 1 }
+  });
 
-    return server.close().then(() => {
-      const completionsResponse = server.getFirstResponseOfType('completions');
-      expect(completionsResponse).toBeDefined();
-    });
+  return server.close().then(() => {
+    const completionsResponse = server.getFirstResponseOfType('completions');
+    t.not(completionsResponse, undefined);
   });
 });
